88a030c3e8d8676ec863a9035a0300e2067ad901
immanuel2006aj-alt <immanuel2006aj@gmail.com>
2026-03-06 22:36:41 +0530

    Refactor call.js to use socket.io for connections
    Replaced RTCPeerConnection with socket.io connection and modified call handling.

--- call.js
+++ call.js
@@ -1,51 +1,11 @@
-const pc = new RTCPeerConnection({
+const socket = io("https://darkcall.onrender.com");
 
-iceServers:[
-{urls:"stun:stun.l.google.com:19302"}
-]
+let myID = localStorage.getItem("userid");
 
-})
+// Register user with server
+socket.emit("register", myID);
 
-let seconds = 0
-
-function startTimer(){
-
-setInterval(()=>{
-
-seconds++
-
-let min = Math.floor(seconds/60)
-let sec = seconds % 60
-
-if(sec < 10) sec = "0"+sec
-if(min < 10) min = "0"+min
-
-document.getElementById("timer").innerText = min+":"+sec
-
-},1000)
-
-}
-
-async function startCall(){
-
-const stream = await navigator.mediaDevices.getUserMedia({audio:true})
-
-stream.getTracks().forEach(track=>{
-
-pc.addTrack(track,stream)
-
-})
-
-startTimer()
-
-}
-
-startCall()
-
-function endCall(){
-
-pc.close()
-
-window.location="dashboard.html"
-
-}
+// Listen for incoming calls
+socket.on("incomingCall", (data) => {
+    alert("Incoming call from " + data.from);
+});
